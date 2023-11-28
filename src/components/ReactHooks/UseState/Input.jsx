df41b6b02fc6455b9fba40cb140d689b4692ac3e
raimundoneto01 <raimundogomes826@gmail.com>
2023-11-27 21:31:05 -0300

    jeicec

---
+++ src/components/ReactHooks/UseState/Input.jsx
@@ -0,0 +1,104 @@
+import { useState } from "react";
+const formFields = [
+  {
+    id: "nome",
+    label: "nome",
+    type: "text",
+  },
+  {
+    id: "sobrenome",
+    label: "sobrenome",
+    type: "text",
+  },
+  {
+    id: "email",
+    label: "email",
+    type: "email",
+  },
+  {
+    id: "bairro",
+    label: "bairro",
+    type: "text",
+  },
+
+  {
+    id: "senha",
+    label: "senha",
+    type: "password",
+  },
+];
+
+export function Input() {
+//   const [form, setForm] = useState(
+//     formFields.reduce((acc, field) => {
+//       return {
+//         ...acc,
+//         [field.id]: "",
+//       };
+//     }, {})
+//   );
+const [user, setUser ] = useState({
+    nome: '',
+    lastNome: '',
+    email: '',
+    bairro: '',
+    cidade: '',  
+})
+
+const meuChange=({target})=>{
+    setUser({...user, [target.id]: target.value})
+}
+//   const handle = ({ target }) => {
+//     const { id, value } = target;
+//     setForm({ ...form, [id]: value });
+//   };
+
+//   const meuSubmite = (event) => {
+//     event.preventDefault();
+//     console.log(form);
+//   };
+  return (
+    <>
+      <h2>linadando com react e usestatte</h2>
+      <div className="felx flex-col  mt-3">
+        <input 
+        className="flex flex-col px-2 my-2 gap-3 border border-black"
+        type="text" value={user.nome} id="nome" name="nome" placeholder="nome:" onChange={meuChange}/>
+
+        <input 
+        className="flex flex-col px-2 my-2 gap-3 border border-black"
+        type="text" value={user.lastNome} id="lastNome" name="lastName" placeholder="SobreNome:" onChange={meuChange} />
+      </div>
+        <input 
+        className="flex flex-col px-2 my-2 gap-3 border border-black"
+        type="email" value={user.email} id="email" name="email" placeholder="E-mail:" onChange={meuChange} />
+
+        <input
+         className="flex flex-col px-2 my-2 gap-3 border border-black" 
+        type="text" value={user.bairro} id="bairro" name="bairro" placeholder="Bairro:" onChange={meuChange} />
+        <input
+         className="flex flex-col px-2 my-2 gap-3 border border-black" 
+        type="text" value={user.cidade} id="cidade" name="cidade" placeholder="Cidade:" onChange={meuChange} />
+
+      {/* <form className="flex flex-col gap-2" onSubmit={meuSubmite}>
+        {formFields.map(({ id, label, type }) => (
+          <div>
+            <label htmlFor={label}>{label}</label>
+            <input
+              className="border border-black rounder px-1 py-1 outline-none"
+              type={type}
+              id={id}
+              placeholder={label}
+              value={form[id]}
+              onChange={handle}
+            />
+          </div>
+        ))}
+        <button className="border border-black bg-green-600  mt-4 py-3 ">
+          Enviar
+        </button>
+      </form> */}
+      
+    </>
+  );
+}
--- src/pages/Hooks/index.jsx
+++ src/pages/Hooks/index.jsx
@@ -1,8 +1,11 @@
-import React, { useState } from "react";
+
+import React, { useState } from "react"; 
+import { Input } from "../../components/ReactHooks/UseState/Input";
 export default function Hooks(){
 
 
     const [ counter, setCounter ] = useState(0);
+    const [ amount, setAmount ] = useState(0)
 
     const handleClick=()=>{
         setCounter((next)=> next + 1)
@@ -10,17 +13,47 @@ export default function Hooks(){
     const handleClick1=()=>{
         setCounter(0)
     }
+
+    const somar=()=>{
+        setCounter(counter +( + amount))
+    }
+
+    const meuClick=()=>{
+        somar()
+    }
+
+
+    const adicionar=()=>{
+        setCounter(counter + (+ amount) + 10)
+    }
+    const remove=()=>{
+        setCounter(counter - 10)
+    }
     return(
         <>
             <div>
                 <h2>useState</h2>
-                <div className="flex gap-2 items-center ">
 
+                <div className="flex gap-2">
+                        <input type=" text" value={amount} onChange={({target})=>setAmount(target.value)} />
+                        <button className="px-2 py-1 border bg-sky-800 rounded text-white"
+                        onClick={meuClick}
+                        >Enviar</button>
+                  </div>
+
+                <div className="flex gap-2 items-center ">
+                    
                     <button className="text-xl" onClick={handleClick}>mais</button>
-                        <p className="text-xl">{counter}</p>
+                        <p className="text-xl">{counter }</p>
                     <button className="text-xl" onClick={handleClick1}>menos</button>
                 </div>
+                <div className="flex gap-3 mt-3 ">
+                    <button onClick={adicionar}>Adicionar</button>
+                    <button onClick={remove}>Remover</button>
+                </div>
+                
             </div>
+            <Input/>
         </>
     );
 }
